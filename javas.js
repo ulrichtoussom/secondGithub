92b65d6924fb29ea6656da3d8802dd4b382df497
ulrich toussom <ulrichtoussom@gmail.com>
2022-12-21 20:49:07 +0100

    ajout du java pour rendre le formulaire dinamyque

---
+++ javas.js
@@ -0,0 +1,65 @@
+
+
+var input = document.getElementsByTagName('input') ;
+var form = document.getElementById('myform')
+var c = input.length ;
+var element = document.getElementById('lastname') ;
+
+function desactivespan(){
+    var spans = document.getElementsByTagName('span') ;
+    var c = spans.length ;
+    for(var i=0 ;i<c ;i++){
+        spans[i].style.display = 'none' ;
+    }
+}
+
+var global ={} ;
+
+global.lastname = function(){
+    var lastname = document.getElementById('lastname') ;
+    var span = lastname.nextElementSibling ;
+    if(lastname.value.length<=4){
+
+        span.style.display='inline-block' ;
+        lastname.className = 'incorect' ;   
+    }else{
+        span.style.display = 'none' ;
+        lastname.className = 'correct';
+    }
+} ;
+
+global.firstname = function(){
+    var firstname = document.getElementById('firstname') ;
+    var span = firstname.nextElementSibling ;
+
+    if(firstname.value.length<=4){
+        span.style.display = 'inline-block' ;
+        firstname.className = 'incorect' ;
+    }else{
+        span.style.display = 'none';
+        firstname.className = 'correct';
+    }
+} ;
+
+global.password = function(){
+
+    var password = document.getElementById('password') ;
+    var span = password.nextElementSibling ;
+
+    if(password.value.length<=6){
+        span.style.display = 'inline-block';
+        password.className = 'incorect';
+    }else{
+        span.style.display = 'none';
+        password.className = 'correct';
+    }
+
+} ;
+
+desactivespan() ;
+form.onsubmit= function(e){
+    e.preventDefault() ;
+    global.lastname() ;
+    global.firstname() ;
+    global.password() ;
+}
